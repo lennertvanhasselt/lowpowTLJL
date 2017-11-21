d48d45527076b74d3da689bc4d7c9bacd7dd9378
Thomas Janssen <thomas.janssen@student.uantwerpen.be>
2017-11-21 22:29:53 +0100

    Wait for 4 D7 messages and write to database

--- FingerprintingD7/DASH7Fingerprinting/DASH7Localization.py
+++ FingerprintingD7/DASH7Fingerprinting/DASH7Localization.py
@@ -1,9 +1,5 @@
 import json
 import paho.mqtt.client as mqtt
-# from scipy.spatial import distance
-# a = (1,2,3)
-# b = (4,5,6)
-# dst = distance.euclidean(a,b)
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -20,31 +16,51 @@ def on_message(client, userdata, msg):
     # print(msg.topic+" "+str(msg.payload))
     data = json.loads(str(msg.payload))
 
-    if data['node'] == '43373134003e0041':                          # Check node ID
-        entry = (data['gateway'], data['link_budget'])              # , data['timestamp']
-        print(entry)
+    global previous_time, reset, rssis, gateways
+    actual_time = float(data['timestamp'][-8:])
+    if previous_time > 8 and actual_time < 2 or previous_time < actual_time - 1:    # kNN: node stuurt bericht --> reset bij ...
+        reset = True                                                                # database creatie: wacht op bericht van GW die nog niet heeft geantwoord
+        previous_time = actual_time
 
+    if reset:
+        del gateways[:]
+
+    elif data['node'] == '43373134003e0041':                          # Check node ID
         if data['gateway'] not in gateways:
-            gateways.append(data['gateway'])
-            matrix.append(entry)
-            write_database(matrix)                                  # write data to database
+            if data['gateway'][0:5] == 'b6b48':
+                gateways.append(data['gateway'])
+                rssis[0] = data['link_budget']
+            elif data['gateway'][0:5] == 'f1f7e':
+                gateways.append(data['gateway'])
+                rssis[1] = data['link_budget']
+            elif data['gateway'][0:5] == 'c2c4e':
+                gateways.append(data['gateway'])
+                rssis[2] = data['link_budget']
+            elif data['gateway'][0:5] == '43e01':
+                gateways.append(data['gateway'])
+                rssis[3] = data['link_budget']
+            else:
+                print('GW not found!')
 
-    if len(matrix) > 4:                                             # wait for 4 messages of different gateways
-                client.disconnect()                                 # and disconnect
+    if len(gateways) == 4:                                          # wait for 4 messages of different gateways,
+        client.disconnect()                                         # disconnect,
+        write_database(rssis)                                       # and write data to database
 
 
-# Write 4 entries to datasbase (1 per GW)
-def write_database(matrix):
+# Write entry to datasbase in the format (nr,RSSI1,RSSI2,RSSI3,RSSI4)
+def write_database(rssis):
     database = open('testdatabase.txt', 'a')                        # Open database
     training_point_number = 1                                       # @ TODO change training point number!!!
-    rssis = str(matrix[0][2]) + ',' + str(matrix[1][2]) + ',' + str(matrix[2][2]) + ',' + str(matrix[3][2])
-    database.write(str(training_point_number) + rssis + '\n')       # Write line: (nr, RSSI1,RSSI2,RSSI3,RSSI4)
+    database.write(str(training_point_number) + rssis[0] + ',' + rssis[1] + ',' + rssis[2] + ',' + rssis[3] + '\n')
     database.close()                                                # Close database
 
 ################################################################
 
-matrix = []
 gateways = []
+rssis = [0, 0, 0, 0]
+previous_time = 0.0
+reset = False
+
 
 client = mqtt.Client(protocol=mqtt.MQTTv31)
 client.on_connect = on_connect
