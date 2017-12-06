484cc6d5c8e185a835500c832f187e2ccbd477ee
jonas <jonas7100@hotmail.com>
2017-12-06 18:59:20 +0100

    eCompass
    eCompass in HAL Work in progress

---
+++ eCompass/CompassHAL/Debug/Src/system_stm32l1xx.d
@@ -0,0 +1,82 @@
+Src/system_stm32l1xx.o: ../Src/system_stm32l1xx.c \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l152xe.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal.h \
+ C:/Users/jonas/workspace/CompassHAL/Inc/stm32l1xx_hal_conf.h \
+ C:/Users/jonas/workspace/CompassHAL/Inc/main.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_def.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc_ex.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_gpio.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_gpio_ex.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_dma.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_cortex.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash_ex.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash_ramfunc.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_i2c.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr_ex.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_tim.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_tim_ex.h \
+ C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_uart.h
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l1xx.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Device/ST/STM32L1xx/Include/stm32l152xe.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/CMSIS/Device/ST/STM32L1xx/Include/system_stm32l1xx.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal.h:
+
+C:/Users/jonas/workspace/CompassHAL/Inc/stm32l1xx_hal_conf.h:
+
+C:/Users/jonas/workspace/CompassHAL/Inc/main.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_def.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_rcc_ex.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_gpio.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_gpio_ex.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_dma.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_cortex.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash_ex.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_flash_ramfunc.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_i2c.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_pwr_ex.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_tim.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_tim_ex.h:
+
+C:/Users/jonas/workspace/CompassHAL/Drivers/STM32L1xx_HAL_Driver/Inc/stm32l1xx_hal_uart.h:
